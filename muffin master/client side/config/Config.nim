6db43d1c24221f4a6af4d5ff543b5ad31b58be96
just patato <123103325+hackpatato@users.noreply.github.com>
2026-09-07 15:56:51 +0300

    Add files via upload

--- muffin master/client side/config/Config.nim
+++ muffin master/client side/config/Config.nim
@@ -1,4 +1,19 @@
-import std/[os,strutils]
-
+import std/[os]
+#for discord
 let BOT_TOKEN* = getEnv("DISCORD_TOKEN", "HERE_TOKEN")
 let CHANNEL_ID* = getEnv("DISCORD_CHANNEL", "HERE_CHANEL")
+#for telegram
+
+###I'll add it later
+
+#for jabber/xmpp
+
+###I'll add it later
+
+#for web handshake 
+
+let SHARED_KEY* = getEnv("shared_keyhex")
+
+##Should I add the Death Drop later?  Maybe in the future(I hope in the very near future)
+
+
---
+++ muffin master/client side/msg/web Handshake/HttpsHandshake.nim
@@ -0,0 +1,7 @@
+import httpclient
+import tls
+import strutils
+import json
+import cyptro/[encrypt, decrypt]
+
+### we gona make shared key things... 
