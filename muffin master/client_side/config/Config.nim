157447524d70413e80f106817abb35e5a941dded
just patato <123103325+hackpatato@users.noreply.github.com>
2026-09-10 14:41:28 +0300

    Update Config.nim

--- muffin master/client_side/config/Config.nim
+++ muffin master/client_side/config/Config.nim
@@ -1,18 +1,4 @@
 import std/[os]
-#for discord
-let BOT_TOKEN* = getEnv("DISCORD_TOKEN", "HERE_TOKEN")
-let CHANNEL_ID* = getEnv("DISCORD_CHANNEL", "HERE_CHANEL")
-#for telegram
-
-###I'll add it later
-
-#for jabber/xmpp
-
-###I'll add it later
-
-#for web handshake 
-
-let SHARED_KEY* = getEnv("shared_keyhex")
 
 ##Should I add the Death Drop later?  Maybe in the future(I hope in the very near future)
 
